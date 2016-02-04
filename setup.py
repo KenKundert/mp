df69344b38139a54fee7b387c14d9fb8b5ed4414
Ken Kundert <thenurd@nurdletech.com>
2016-02-04 01:49:59 -0800

    Improved setup.py file.

--- setup.py
+++ setup.py
@@ -1,7 +1,18 @@
 from setuptools import setup
-import manpage
 from music_player import version
-manpage.write()  # generate the nroff version of the manpage
+
+# Create/update manpage before installing                                      
+message = ''
+try:
+    import manpage
+    manpage.write()  # generate the nroff version of the manpage
+except ImportError:                                                            
+    # this will let the setup script run, allowing prerequisites to be 
+    # installed, but then setup script must be run again so manpage is 
+    # generated.
+    with open('fvi.1', 'w') as f:                                              
+        f.write('')
+    message='\nRerun setup in order to generate the manpage.'
 
 with open('README.rst') as f:
     longDescription=f.read()
@@ -20,5 +31,20 @@ setup(
     ]
   , platforms=['rhel']
   , use_2to3=True
-  , license='GPLv3'
+  , license='GPLv3+'
+  , install_requires=[
+        'mutagen',
+    #    'gi',
+    ],
+    # there seems to be a problem with the python3 version of gi. If you allow 
+    # python to install gi itself, it seems to get the python2 version. You 
+    # need to use 'yum install python3-gi', and so you need to be using the 
+    # system installed version of python3.
+    # So to install this package into the system version of python3:
+    # 1. comment out 'gi' in install_requires above
+    # 2. manually install scripts package using /usr/bin/python3
+    # 3. modify install so it installs mp using /usr/bin/python3
+    # 4. run ./install
 )
+
+print(message)
