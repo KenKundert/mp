41e63003a94171ef3c682ec56ff1fc3d5d2ff198
Ken Kundert <thenurd@nurdletech.com>
2016-06-17 19:24:18 -0700

    Bump version: 1.3.0 → 1.3.1

--- music_player/prefs.py
+++ music_player/prefs.py
@@ -4,6 +4,6 @@ mediaFileExtensions = ['.flac', '.mp3', '.ogg', '.wav'] # use lower case only
 restartFilename = '.mp-restart'
 nowPlayingFilename = '~/.nowplaying'
 separator = '### skip the following songs ###'
-version = "1.3.0"
+version = "1.3.1"
 date = "2016-06-17"
 skipSongThatWasPlayingWhenLastKilled = False
